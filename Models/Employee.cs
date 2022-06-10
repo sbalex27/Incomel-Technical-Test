d9d2b398f1b1939c1388f8c6de8938df3ee36671
Alexander Batres <alex2000.sabe@gmail.com>
2022-06-10 09:57:28 -0600

    Employee props display name

--- Models/Employee.cs
+++ Models/Employee.cs
@@ -1,14 +1,23 @@
-﻿namespace Incomel_Technical_Test.Models
+﻿using System.ComponentModel;
+
+namespace Incomel_Technical_Test.Models
 {
     public class Employee
     {
         public int Id { get; set; }
+        [DisplayName("DPI")]
         public String Dpi { get; set; }
+        [DisplayName("Nombre Completo")]
         public String FullName { get; set; }
+        [DisplayName("Cantidad de Hijos")]
         public int ChildCount { get; set; }
+        [DisplayName("Sueldo")]
         public float Salary { get; set; }
+        [DisplayName("Bono")]
         public float Bonus { get; set; }
+        [DisplayName("ID Usuario")]
         public int UserId { get; set; }
+        [DisplayName("Creado")]
         public DateTime CreatedAt { get; set; }
 
         public Employee()
