33f38c41117be7f77994ddd265b83630d14cf6ab
Alexander Batres <alex2000.sabe@gmail.com>
2022-06-09 20:26:10 -0600

    Agregar archivos de proyecto.

---
+++ Models/Employee.cs
@@ -0,0 +1,18 @@
+﻿namespace Incomel_Technical_Test.Models
+{
+    public class Employee
+    {
+        public String Dpi { get; set; }
+        public String FullName { get; set; }
+        public int ChildCount { get; set; }
+        public float Salary { get; set; }
+        public float Bonus { get; set; }
+        public int UserId { get; set; }
+        public DateTime CreatedAt { get; set; }
+
+        public Employee()
+        {
+
+        }
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Incomel_Technical_Test.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
