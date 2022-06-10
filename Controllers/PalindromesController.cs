0fa2f0b748afbe4271113dc8b7f99146c2bfd558
Alexander Batres <alex2000.sabe@gmail.com>
2022-06-10 16:39:08 -0600

    Palíndromos

---
+++ Controllers/PalindromesController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Incomel_Technical_Test.Controllers
+{
+    public class PalindromesController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,31 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+function palindrome(inputText) {
+    var lower = inputText.toLowerCase();
+    var reverse = lower.split('').reverse().join('');
+    return reverse === lower;
+}
+
+function palindromes() {
+    var input = document.getElementById("palindromes-input").value.toLowerCase();
+    var list = document.getElementById("item-list");
+    var results = [];
+    list.innerHTML = "";
+
+    console.log(input.length)
+
+    for (var i = 3; i < input.length; i++) {
+
+        for (var length = 0; length <= input.length; length++) {
+            var sliced = input.substr(length, i);
+            if (palindrome(sliced) && !results.includes(sliced) && sliced.length >= 3) {
+                results.push(sliced)
+                var element = document.createElement("li");
+                element.innerHTML = sliced;
+                list.appendChild(element);
+            }
+        }
+    }
+}
